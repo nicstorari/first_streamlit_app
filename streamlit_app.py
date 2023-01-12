17559701041dbc0f7cc5f6c4a958c37b6e6dee6e
nicstorari <122300820+nicstorari@users.noreply.github.com>
2023-01-12 17:28:40 +0100

    Update streamlit_app.py
    added a stop to snowflake cursor code
    added ELSEIF to determine whether or not to fetch data

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,21 +29,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-#New section to display fruityvicy api response
+# New section to display fruityvicy api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-# use request library
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-#streamlit.text(fruityvice_response.json())  #just writes data on the screen
-
-# normalize json file
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# show normalized json file in dataframe
-streamlit.dataframe(fruityvice_normalized)
-
-
+except URLError as e:
+  streamlit.error()
+  
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) #uses connection settings in app secrets
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
